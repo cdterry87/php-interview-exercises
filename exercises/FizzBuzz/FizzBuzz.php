25ba31304cf40cd917aa263289ce56e25f780b94
Chase Terry <chase.terry87@gmail.com>
2021-06-14 21:53:04 -0500

    Completed FizzBuzz exercise

--- exercises/FizzBuzz/FizzBuzz.php
+++ exercises/FizzBuzz/FizzBuzz.php
@@ -15,4 +15,24 @@ namespace Exercises\FizzBuzz;
  */
 final class FizzBuzz
 {
+  public static function print(int $limit): void
+  {
+    $word = '';
+    for($i = 1; $i <= $limit; ++$i) {
+      $isDivisibleBy3 = $i % 3 === 0;
+      $isDivisibleBy5 = $i % 5 === 0;
+
+      if ($isDivisibleBy3) {
+        $word .= 'fizz';
+      } 
+      if ($isDivisibleBy5) {
+        $word .= 'buzz';
+      }
+
+      if (!$isDivisibleBy3 && !$isDivisibleBy5) {
+        $word .= $i;
+      }
+    }
+    echo $word;
+  }
 }
